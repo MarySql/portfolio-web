ec217695a6c87678a980ed0488ff4cf1bc92a830
MarySql <mary.mitsuri250@gmail.com>
2024-11-03 18:49:58 -0300

    settings

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/portifolio-web/',
+  base: './', // Isso ajusta os caminhos para serem relativos ao arquivo index.html
 });
