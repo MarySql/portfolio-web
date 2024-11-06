95e881a02058955473b09da075fdaa58f9e9d024
MarySql <mary.mitsuri250@gmail.com>
2024-11-06 14:47:31 -0300

    js-ajust

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ import './styles/footer.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 // Routes
-import { renderHeader } from '../portfolio-web/routes/header.js';
-import { renderMain } from '../portfolio-web/routes/main.js';
-import { renderAbout } from '../portfolio-web/routes/about.js';
+import { renderHeader } from './routes/header.js';
+import { renderMain } from '.routes/main.js';
+import { renderAbout } from './routes/about.js';
 import { renderArticle } from './routes/article.js';
 import { renderFooter } from './routes/footer.js';
 
